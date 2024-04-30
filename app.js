e053b4bf8800d250183c4269d912a320e7505625
Lorena C <criado.manzaneque@gmail.com>
2024-04-30 21:24:40 +0200

    Start the exercice

---
+++ app.js
@@ -0,0 +1,17 @@
+
+function changeColor() {
+    document.getElementById('square-section').style.backgroundColor = "#F9F7C9"
+}
+
+function changeCurrentColor() {
+    document.getElementById('square-section').style.backgroundColor = "#80BCBD"
+}
+
+function changeImage() {
+    document.getElementById("principal-image").src = "images/pikachu-gogh.jpg"
+}
+
+function changeCurrentImage() {
+    document.getElementById("principal-image").src = "images/almendros.jpg"
+}
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Ejercicio de Eventos en JavaScript
+Este es un pequeño proyecto para explorar diferentes eventos en JavaScript y cómo se pueden utilizar para interactuar con los elementos HTML en una página web.
+
+## Descripción
+El ejercicio consiste en una página web sencilla que contiene diferentes elementos HTML con los que interactuar a través de eventos de JavaScript. Cada evento realiza una acción diferente cuando se activa.
